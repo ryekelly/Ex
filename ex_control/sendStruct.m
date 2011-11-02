942e7fe2652ffed9e939b5ef486cc9e3c3d4c487
Matt Smith <matadasm@gmail.com>
2011-11-02 17:37:06 -0500

    changes to sendStruct

--- ex_control/sendStruct.m
+++ ex_control/sendStruct.m
@@ -1,9 +1,17 @@
+%function sendStruct(s)
+%
+% Send a struct as ascii over the digital port using digCode.m
+%
+%
 function sendStruct(s)
 
 fields = fieldnames(s);
 
 for i = 1:length(fields)
     val = num2str(s.(fields{i}));
-    m = double([fields{i} '=' val])*256;
-    digCode([m 256]);
+    % Ryan's original
+    %m = double([fields{i} '=' val])*256;
+    %digCode([m 256]);
+    m = double([fields{i} '=' val])+256;
+    digCode(m);
 end
