91ffcde651f63be12cc32a377d2c4bf059e97947
Matt Smith <matadasm@gmail.com>
2012-04-12 13:19:38 -0400

    Updated sendStruct so that it writes into allCodes, and respects the params.sendingCodes flag

--- ex_control/sendStruct.m
+++ ex_control/sendStruct.m
@@ -1,21 +1,21 @@
+function sendStruct(s)
 %function sendStruct(s)
 %
 % Send a struct as ascii over the digital port using digCode.m
 %
-%
-function sendStruct(s)
 
-global params
+global params thisTrialCodes trialTic
 
 fields = fieldnames(s);
 
 for i = 1:length(fields)
     val = num2str(s.(fields{i}));
-    % Ryan's original
-    %m = double([fields{i} '=' val])*256;
-    %digCode([m 256]);
     m = double([fields{i} '=' val ';'])+256;
+    
     if params.sendingCodes
         digCode(m);
     end
+    
+    thisTrialCodes(end+1:end+length(m),:) = [m(:) ones(length(m),1).*toc(trialTic)];
 end
+
